300f878bb979c01796fbff8a2a4ecb44d2860ee2
michaelayoola <mayoola53@yahoo.co.uk>
2019-12-24 16:52:35 +0000

    Initial commit

---
+++ src/main/java/org/michael/services/MainBootStarter.java
@@ -0,0 +1,14 @@
+package org.michael.services;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBootStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainBootStarter.class, args);
+    }
+
+}
